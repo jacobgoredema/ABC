e964c4c14df56bd40467abf9250ee9622ac28e61
jacob goredema <jacobgoredema@gmail.com>
2020-05-08 22:23:13 +0200

    added fluent validation api

---
+++ ABC.BusinessLayer/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,29 @@
+﻿using ABC.Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BusinessLayer.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+
+        public ProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p=>p.CategoryId==1);
+            RuleFor(p => p.Name).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
